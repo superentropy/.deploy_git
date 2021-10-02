b4fc07ae6cf5897fd9173ca165eb2d5c8311fc7a
superentropy <zhangnoah@163.com>
2021-10-02 09:28:00 +0800

    Site updated: 2021-10-02 09:27:59

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "4A86E8",
+        "tmpSize": "16",
+        "cityColor": "FF9900",
+        "citySize": "16",
+        "aqiColor": "4A86E8",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "0px 0px 0px 0px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "true",
+        "vertical": "center",
+        "horizontal": "center",
+        "left": "100",
+        "top": "20",
+        "key": "8eb2a8f3608f49ca99d69eff5cb971c0"
+    }
+}
